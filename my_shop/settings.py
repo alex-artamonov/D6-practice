cb19538fae7720a67ff4de31057419e6923355cb
alex-artamonov <alex-artamonov2010@gmail.com>
2022-09-24 00:09:10 +0300

    add default group for new users

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -131,6 +131,7 @@ ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_FORMS = {'signup': 'news.forms.BasicSignupForm'}
 
 
 # Internationalization
--- news/forms.py
+++ news/forms.py
@@ -1,4 +1,6 @@
 from django.forms import ModelForm
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
 from .models import Post
 
 
@@ -6,3 +8,12 @@ class NewsForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'author', 'content', 'type', 'categories']
+
+
+class BasicSignupForm(SignupForm):
+    
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        basic_group = Group.objects.get(name='common')
+        basic_group.user_set.add(user)
+        return user

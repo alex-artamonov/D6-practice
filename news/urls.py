b9beda396e09f8f541b7f95586aea748d7bbe4e6
alex-artamonov <alex-artamonov2010@gmail.com>
2022-09-24 20:37:12 +0300

    add button upgrade to authors

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from .views import NewsList, NewsDetail, NewsF , NewsCreate, NewsDelete, NewsUpdate
+from .views import NewsList, NewsDetail, NewsF , NewsCreate, NewsDelete, NewsUpdate, upgrade_me
+from .views import DefaultView
  
 urlpatterns = [
     # path — означает путь. В данном случае путь ко всем товарам у нас останется пустым, 
@@ -11,4 +12,6 @@ urlpatterns = [
     path('add/', NewsCreate.as_view(), name='add_news'),
     path('<int:pk>/delete/', NewsDelete.as_view(), name='delete_news'),
     path('<int:pk>/edit/', NewsUpdate.as_view(), name='edit_news'),
+    path('upgrade/', upgrade_me, name = 'upgrade'),
+    path('default/', DefaultView.as_view()),
     ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,11 @@
-from operator import methodcaller
-from django.http import HttpResponse
+# from operator import methodcaller
+# from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 from .models import *
 from .filters import NewsFilter
@@ -15,6 +19,12 @@ class NewsList(LoginRequiredMixin, ListView):
     template_name = 'news/news.html'
     context_object_name = 'news'
     paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_author'] = not self.request.user.groups.filter(name = 'authors').exists()
+        # context['one'] = 'zwei'
+        return context
 
 class NewsDetail(LoginRequiredMixin, DetailView):
     model = Post
@@ -67,6 +77,26 @@ class NewsF(LoginRequiredMixin, ListView):
         authors = Author.objects.all()
         context['authors'] = authors
         return context
+
+
+
+class DefaultView(LoginRequiredMixin, TemplateView):
+    template_name = 'flatpages/default.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_author'] = not self.request.user.groups.filter(name = 'authors').exists()
+        context['one'] = 'first'
+        return context
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        authors_group.user_set.add(user)
+    return redirect('/')
         
 
     # def get_context_data(self, **kwargs): # забираем отфильтрованные объекты переопределяя 
